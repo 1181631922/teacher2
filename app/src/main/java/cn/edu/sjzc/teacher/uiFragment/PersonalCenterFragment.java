cdd509794283807521df56b5699297e4ed8a03c2
樊亚风 <1181631922@qq.com>
2015-05-16 19:22:24 +0800

    2015/5/16号提交，个人信息中教工号可以得到

--- app/src/main/java/cn/edu/sjzc/teacher/uiFragment/PersonalCenterFragment.java
+++ app/src/main/java/cn/edu/sjzc/teacher/uiFragment/PersonalCenterFragment.java
@@ -1,6 +1,7 @@
 package cn.edu.sjzc.teacher.uiFragment;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,8 +9,10 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import cn.edu.sjzc.teacher.R;
+import cn.edu.sjzc.teacher.app.UserApplication;
 import cn.edu.sjzc.teacher.uiActivity.AboutActivity;
 import cn.edu.sjzc.teacher.uiActivity.PerChangeInfoActivity;
 import cn.edu.sjzc.teacher.uiActivity.PerChangePasswordActivity;
@@ -17,7 +20,6 @@ import cn.edu.sjzc.teacher.uiActivity.PerPersonalInfoActivity;
 import cn.edu.sjzc.teacher.uiActivity.PerScheduleActivity;
 
 public class PersonalCenterFragment extends BaseFragment implements OnClickListener {
-
     public static BaseFragment newInstance(int index) {
         BaseFragment fragment = new PersonalCenterFragment();
         Bundle args = new Bundle();
@@ -30,35 +32,38 @@ public class PersonalCenterFragment extends BaseFragment implements OnClickListe
     private Button user_but, per_person_but, per_password_but,
             per_schedule_but, per_about__but;
     private ImageView redpoint;
-
+    private TextView per_studentid_text;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_personalcenter, container, false);
-
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
         initView();
+        initData();
+    }
 
+    private void initData() {
+        SharedPreferences userdata = getActivity().getSharedPreferences(UserApplication.USER_DATA, 0);
+        String number = userdata.getString(UserApplication.USER_DATA_NUMBER, "");
+        String name = userdata.getString(UserApplication.USER_DATA_USERNAME, "");
+        this.per_studentid_text.setText("老师教工号：" + number);
+        this.user_but.setText(name);
     }
 
     public void initView() {
-//        this.redpoint = (ImageView) getActivity().findViewById(R.id.redpoint);
-//        this.redpoint.setVisibility(View.GONE);
-        Button user_but = (Button) getActivity().findViewById(R.id.user_but);
-        user_but.setOnClickListener(this);
-        Button per_person_but = (Button) getActivity().findViewById(R.id.per_person_but);
-        per_person_but.setOnClickListener(this);
-        Button per_password_but = (Button) getActivity().findViewById(R.id.per_password_but);
-        per_password_but.setOnClickListener(this);
-        Button per_schedule_but = (Button) getActivity().findViewById(R.id.per_schedule_but);
-        per_schedule_but.setOnClickListener(this);
+        this.per_studentid_text = (TextView) getActivity().findViewById(R.id.per_studentid_text);
+        this.user_but = (Button) getActivity().findViewById(R.id.user_but);
+        this.user_but.setOnClickListener(this);
+        this.per_person_but = (Button) getActivity().findViewById(R.id.per_person_but);
+        this.per_person_but.setOnClickListener(this);
+        this.per_password_but = (Button) getActivity().findViewById(R.id.per_password_but);
+        this.per_password_but.setOnClickListener(this);
+        this.per_schedule_but = (Button) getActivity().findViewById(R.id.per_schedule_but);
+        this.per_schedule_but.setOnClickListener(this);
         this.per_about__but = (Button) getActivity().findViewById(R.id.per_about__but);
         this.per_about__but.setOnClickListener(this);
     }
